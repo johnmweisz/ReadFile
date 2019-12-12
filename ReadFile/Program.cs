0431310c7efb134683d18da41300c29c90d91145
John Weisz <johnmweisz@gmail.com>
2019-12-12 14:59:09 -0800

    Add project files.

---
+++ ReadFile/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberOfWords(@"C:\Users\johnm\Desktop\test.txt"));
+            Console.WriteLine(LongestWord(@"C:\Users\johnm\Desktop\test.txt"));
+        }
+
+        static int NumberOfWords(string path)
+        {
+            var allWords = File.ReadAllText(path, Encoding.Default);
+            var wordArr = allWords.Split(' ');
+            return wordArr.Length;
+        }
+
+        static string LongestWord(string path)
+        {
+            var allWords = File.ReadAllText(path, Encoding.Default);
+            var wordArr = allWords.Split(' ');
+            var longestWord = "";
+            foreach (var word in wordArr)
+            {
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+            }
+            return longestWord;
+        }
+    }
+}
